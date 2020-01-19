27468f8886f0ccdfad7170f5edfd490f112167b0
Kacper Jeczmien <kacperj9713@gmail.com>
2020-01-19 19:23:51 +0100

    AudioAdapter finished

--- src/MediaAdapter.java
+++ src/MediaAdapter.java
@@ -2,12 +2,11 @@ public class MediaAdapter implements MediaPlayer {
 
     AdvancedMediaPlayer advancedMusicPlayer;
 
-    public MediaAdapter(String audioType){
+    public MediaAdapter(String audioType) {
 
-        if(audioType.equalsIgnoreCase("vlc")) {
+        if (audioType.equalsIgnoreCase("vlc")) {
             advancedMusicPlayer = new VlcPlayer();
-        }
-        else if (audioType.equalsIgnoreCase("mp4")) {
+        } else if (audioType.equalsIgnoreCase("mp4")) {
             advancedMusicPlayer = new Mp4Player();
         }
     }
@@ -15,10 +14,9 @@ public class MediaAdapter implements MediaPlayer {
     @Override
     public void play(String audioType, String fileName) {
 
-        if(audioType.equalsIgnoreCase("vlc")){
+        if (audioType.equalsIgnoreCase("vlc")) {
             advancedMusicPlayer.playVlc(fileName);
-        }
-        else if(audioType.equalsIgnoreCase("mp4")){
+        } else if (audioType.equalsIgnoreCase("mp4")) {
             advancedMusicPlayer.playMp4(fileName);
         }
     }
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Mp4Player MP4 = new Mp4Player();
+        MP4.playMp4("Biały miś");
+    }
+}
